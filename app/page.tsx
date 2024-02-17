e9dbc6c8c92636e7663fac6788ce5e19fd954f4c
Victor Prado Trujillo <vicknan0182@gmail.com>
2024-02-17 13:34:16 -0600

    routing set

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className='h1-bold'>Hello Next14. I&apos;m gonna master it! </h1>
-      <h2 className='h2-bold'>Hello Next14. I&apos;m gonna master it! </h2>
-      <h3 className='h3-bold'>Hello Next14. I&apos;m gonna master it! </h3>
-    </div>
-  )
-}
